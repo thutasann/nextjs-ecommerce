fde2344962a55923001878162c5896633f7b0361
thuta_yoma <thutasann@yomabank.com>
2022-09-24 18:44:54 +0630

    next js configuration modified

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+  devIndicators: {
+    buildActivity: false
+  }
 }
 
 module.exports = nextConfig
